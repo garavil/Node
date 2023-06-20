2d23c1d327ec78b27ba1eda30671f7d7e22011db
Gara Vilches <garavilches@gmail.com>
2023-06-20 11:15:06 +0100

    reto2

---
+++ dia3/reto2/src/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const ruta = require("./routers/user.routers");
+const errorHandling = require("./error/errorHandling");
+const app = express();
+
+app.set("port", process.env.PORT || 3000)
+
+app.use(cors());
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+app.use(ruta);
+app.use(function(req, res, next){
+    res.status(404).json({error:true,
+                        codigo:400,
+                        message: "Endpoint doesnt found"})
+})
+
+app.use(errorHandling);
+
+module.exports = app;
